82178d07636efd4b8e3fc8da1a96e2f781a4cc4f
Your Name <you@example.com>
2023-10-09 16:37:04 +0600

    added google map

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -2,56 +2,66 @@ import React from "react";
 
 const about = () => {
   return (
-    <div className="bg-gray-100 p-6">
-      <h1 className="text-4xl font-semibold mb-4">
-        About Us - Corporate Events
-      </h1>
-      <p className="text-lg mb-4">
-        We are your go-to destination for a wide range of corporate events,
-        specializing in the following categories:
-      </p>
-      <ul className="list-disc pl-6 mb-4">
-        <li>
-          <strong className="text-indigo-600">Conferences and Seminars:</strong>{" "}
-          Providing valuable knowledge-sharing and networking opportunities for
-          professionals.
-        </li>
-        <li>
-          <strong className="text-indigo-600">Trade Shows and Expos:</strong>{" "}
-          Showcasing the latest products and services in your industry to a wide
-          audience.
-        </li>
-        <li>
-          <strong className="text-indigo-600">Team Building Workshops:</strong>{" "}
-          Strengthening teamwork and collaboration within your organization
-          through engaging workshops.
-        </li>
-        <li>
-          <strong className="text-indigo-600">Product Launches:</strong>{" "}
-          Creating buzz and excitement around your new products or services.
-        </li>
-        <li>
-          <strong className="text-indigo-600">Corporate Meetings:</strong>{" "}
-          Facilitating important discussions and decision-making among key
-          stakeholders.
-        </li>
-        <li>
-          <strong className="text-indigo-600">Award Ceremonies:</strong>{" "}
-          Recognizing and celebrating outstanding achievements within your
-          company or industry.
-        </li>
-        {/* Add more categories as needed */}
-      </ul>
-      <p className="text-lg mb-4">
-        Our team is dedicated to delivering exceptional event experiences that
-        meet your specific needs. Whether it's a large-scale conference or an
-        intimate team-building workshop, we are here to make your corporate
-        events a resounding success.
-      </p>
-      <p className="text-lg">
-        Contact us today to discuss how we can help you plan and execute
-        memorable corporate events that leave a lasting impact.
-      </p>
+    <div className="bg-base-100 min-h-screen">
+      {/* Header */}
+      <header className="bg-gray-700 text-white py-6">
+        <div className="container mx-auto text-center">
+          <h1 className="text-4xl font-bold">About Us</h1>
+          <p className="mt-2">Learn more about our organization.</p>
+        </div>
+      </header>
+
+      {/* Introduction */}
+      <section className="py-10">
+        <div className="container mx-auto text-white font-bold">
+          <h2 className="text-2xl font-semibold mb-4">Who We Are</h2>
+          <p>
+            We are a passionate team dedicated to [Your Organization's Mission].
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            facilisi. Vivamus ut ante auctor, congue lorem eget, bibendum elit.
+          </p>
+        </div>
+      </section>
+
+      {/* Our History */}
+      <section className="bg-gray py-10">
+        <div className="container mx-auto text-white font-bold">
+          <h2 className="text-2xl font-semibold mb-4">Our History</h2>
+          <p>
+            Our journey began in [Year of Establishment]. Since then, we have
+            achieved [Milestones and Achievements]. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Nulla facilisi.
+          </p>
+        </div>
+      </section>
+
+      {/* Our Team */}
+      <section className="py-10">
+        <div className="container mx-auto text-white font-bold">
+          <h2 className="text-2xl font-semibold mb-4">Our Team</h2>
+          <p>
+            Meet the talented individuals behind [Your Organization's Name].
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+            facilisi. Vivamus ut ante auctor, congue lorem eget, bibendum elit.
+          </p>
+          {/* Add team members' profiles, photos, and descriptions here */}
+        </div>
+      </section>
+
+      {/* Mission and Values */}
+      <section className="bg-gray py-10">
+        <div className="container mx-auto text-white font-bold">
+          <h2 className="text-2xl font-semibold mb-4">
+            Our Mission and Values
+          </h2>
+          <p>
+            At [Your Organization's Name], our mission is to [State Your
+            Mission]. We uphold core values such as [Your Core Values] in
+            everything we do. Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit. Nulla facilisi.
+          </p>
+        </div>
+      </section>
     </div>
   );
 };
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -6,8 +6,10 @@ const Card = ({ cardData }) => {
       <img className="w-full" src={cardData.picture} alt={cardData.title} />
       <div className="px-6 py-4">
         <h2 className="font-bold text-xl mb-2">{cardData.title}</h2>
-        <p className="text-gray-700 text-base">{cardData.description}</p>
-        <p className="text-gray-900 text-lg mt-2">Price: ${cardData.price}</p>
+        <p className="text-gray-500 text-base">{cardData.description}</p>
+        <p className="text-white text-lg font-bold mt-2">
+          Price: ${cardData.price}
+        </p>
       </div>
       <div className="px-6 py-4">
         <a
